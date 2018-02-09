4e13d2c2bf4096748cd3f7aed98fbf24dad35faa
sudhir nimavat <sudhir@nimavat.me>
2018-02-09 16:28:03 +0530

    update travis publish, remove code related to building and publishing docs

--- travis-publish.sh
+++ travis-publish.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "yakworks/view-tools" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "yakworks/quartz-config-scheduler" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
     if [[ -n $TRAVIS_TAG ]]
     then
@@ -11,41 +11,6 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG ==
     else
          echo "### publishing snapshot"
         ./gradlew view-tools:publish --no-daemon
-    fi
-
-    if [[ $TRAVIS_BRANCH == 'master' ]]
-    then
-
-        echo "### publishing docs"
-        git config --global user.name "9cibot"
-        git config --global user.email "9cibot@9ci.com"
-        git config --global credential.helper "store --file=~/.git-credentials"
-        echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
-        git clone https://${GITHUB_TOKEN}@github.com/yakworks/view-tools.git -b gh-pages gh-pages --single-branch > /dev/null
-
-        python3 -m mkdocs build
-
-        cd gh-pages
-        cp -r ../site/. . #Copy Mkdocs
-
-        mkdir -p api #Copy Java API
-        cp -r ../plugin/build/docs/groovydoc/. ./api
-
-        git add .
-
-        #If there are any changes, do commit and push
-        if [[ -n $(git status -s) ]]
-        then
-            git commit -a -m "Update docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-            git push origin HEAD
-        else
-            echo "### No changes to docs"
-        fi
-
-        cd ..
-        rm -rf gh-pages
-        rm -rf site
-        echo "### Done publishing docs"
     fi
 
 else
